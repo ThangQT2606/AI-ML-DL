78a2fe5e34bb1151b6a0f2d2aa87ea037ce9e06a
Đinh Thắng <quangthang260602@gmail.com>
2024-06-20 21:17:24 +0700

    add YOLO

--- ONNX-YOLOv8-Object-Detection-main/models/convert_modelYOLO_ONNX.py
+++
@@ -1,9 +0,0 @@
-import os, onnx
-from ultralytics import YOLO
-
-# Đường dẫn model YOLO
-model_path = os.path.join(os.path.dirname(__file__), 'best.pt')
-model = YOLO(model_path)
-
-# convert Yolo to onnx
-model.export(format="onnx")
